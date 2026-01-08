4e596b2b422ff764862ebe3473dee94ceed14637
Azarele <36418994+Azarele@users.noreply.github.com>
2026-01-08 20:04:03 +0000

    Update login.py

--- modules/login.py
+++ modules/login.py
@@ -2,43 +2,46 @@ import streamlit as st
 import auth_utils as auth
 
 def show():
-    # Use columns to constrain the width of the login form
+    # Center the login box
     c1, c2, c3 = st.columns([1, 2, 1])
     
     with c2:
-        # --- LOGO CENTERING ---
-        # Create 3 sub-columns and place image in the middle one to center it.
-        sc1, sc2, sc3 = st.columns([1, 0.5, 1])
-        with sc2:
-            try:
-                # use_container_width makes it fill the middle column
-                st.image("nync_marketing.png", width='stretch') 
-            except:
-                pass
-
-        # TEXT BELOW LOGO
-        st.markdown("<h1 style='text-align: center; margin-top: 0px;'>Nync.</h1>", unsafe_allow_html=True)
-        st.markdown("<p style='text-align: center; color: #888;'>Stop maximizing convenience. Start minimizing pain.</p>", unsafe_allow_html=True)
+        st.header("⚡ Nync")
+        st.write("Stop playing Calendar Tetris.")
         
-        st.write("") # Spacer
+        # --- GOOGLE LOGIN (FIXED: SAME TAB) ---
+        google_url = auth.get_google_url()
+        if google_url:
+            # We use HTML to force target="_self" so it doesn't open a new tab
+            st.markdown(f'''
+                <a href="{google_url}" target="_self" style="
+                    display: inline-block;
+                    width: 100%;
+                    background-color: #2e2e2e;
+                    color: white;
+                    text-align: center;
+                    padding: 10px 0px;
+                    border-radius: 8px;
+                    text-decoration: none;
+                    border: 1px solid #444;
+                    font-family: sans-serif;
+                    margin-bottom: 15px;
+                ">
+                    Sign in with Google
+                </a>
+            ''', unsafe_allow_html=True)
 
-        tab1, tab2 = st.tabs(["Sign In", "Sign Up"])
-        
-        with tab1:
-            with st.form("login_form"):
-                email = st.text_input("Email")
-                password = st.text_input("Password", type="password")
-                if st.form_submit_button("Log In", type="primary", width="stretch"):
-                    auth.login_user(email, password)
+        st.markdown("--- OR ---")
+
+        # --- EMAIL LOGIN ---
+        with st.form("login_form"):
+            email = st.text_input("Email")
+            password = st.text_input("Password", type="password")
             
-            st.write("---")
-            google_url = auth.get_google_url()
-            if google_url:
-                st.link_button("🔵 Sign in with Google", google_url, use_container_width=True)
-
-        with tab2:
-            with st.form("signup_form"):
-                email = st.text_input("Email")
-                password = st.text_input("Password", type="password")
-                if st.form_submit_button("Create Account", width="stretch"):
+            b1, b2 = st.columns(2)
+            with b1:
+                if st.form_submit_button("Log In", use_container_width=True):
+                    auth.login_user(email, password)
+            with b2:
+                if st.form_submit_button("Sign Up", use_container_width=True):
                     auth.signup_user(email, password)
